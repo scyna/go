5f11d4e8c030cea7f2fb03d3ab3bea7ad7125db0
KhanhND <khanhnd162@gmail.com>
2022-04-22 10:50:30 +0700

    implement add, get activity

--- activity.go
+++ activity.go
@@ -1,6 +1,7 @@
 package scyna
 
 import (
+	"github.com/scylladb/gocqlx/v2"
 	"strings"
 	"sync"
 	"time"
@@ -64,4 +65,43 @@ func (stream *ActivityStream) Add(entity uint64, Type int, event protoreflect.Pr
 		}
 		t++
 	}
+}
+
+func (stream *ActivityStream) AddStringData(entity uint64, Type string, data string) {
+	t := uint64(time.Now().UnixMicro())
+
+	qInsert := stream.Queries.GetQuery()
+	defer stream.Queries.Put(qInsert)
+
+	for i := 0; i < tryCount; i++ {
+		qInsert.Bind(entity, Type, t, data)
+		if applied, err := qInsert.ExecCAS(); applied {
+			return
+		} else {
+			if err != nil {
+				LOG.Error("EventStream.AddJsonData :" + err.Error())
+				return
+			}
+		}
+		t++
+	}
+}
+
+func GetActivityQuery(name string, entity uint64) *gocqlx.Queryx {
+	return qb.Select(name).
+		Columns("entity_id", "type", "time", "data").
+		Where(qb.Eq("entity_id")).
+		Query(DB).
+		Bind(entity)
+}
+
+func (stream *ActivityStream) Get(entity uint64) []Activity {
+	tName := strings.Split(stream.Entity, ".")[0] + ".es_" + strings.Split(stream.Entity, ".")[1] + "_" + stream.Stream
+	qSelect := GetActivityQuery(tName, entity)
+	var event []Activity
+	if err := qSelect.Select(&event); err != nil {
+		LOG.Error("Can not get event: " + err.Error())
+	}
+
+	return event
 }
