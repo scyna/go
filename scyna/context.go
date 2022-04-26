f577e4870ca74b8ba212b31ef1c0c8877cb2dddb
NGUYEN Hong San <hongsan@gmail.com>
2022-04-26 18:10:12 +0700

    add AuthDone

--- scyna/context.go
+++ scyna/context.go
@@ -48,6 +48,40 @@ func (ctx *Context) Done(r proto.Message) {
 	ctx.flush(&response)
 }
 
+func (ctx *Context) AuthDone(r proto.Message, token string, expired uint64) {
+	response := Response{Code: 200, Token: token, Expired: expired}
+
+	var err error
+	if ctx.Request.JSON {
+		response.Body, err = json.Marshal(r)
+	} else {
+		response.Body, err = proto.Marshal(r)
+	}
+	if err != nil {
+		response.Code = int32(500)
+		response.Body = []byte(err.Error())
+	}
+
+	ctx.flush(&response)
+}
+
+// func (s *Context) Auth(org string, secret string, apps []string, userID string) (bool, string) {
+// 	request := CreateAuthRequest{
+// 		Organization: org,
+// 		Secret:       secret,
+// 		Apps:         apps,
+// 		UserID:       userID,
+// 	}
+
+// 	var response CreateAuthResponse
+// 	if err := CallService(AUTH_CREATE_URL, &request, &response); err != OK {
+// 		return false, ""
+// 	}
+// 	s.Response.Token = response.Token
+// 	s.Response.Expired = response.Expired
+// 	return true, response.Token
+// }
+
 func (ctx *Context) flush(response *Response) {
 	response.SessionID = Session.ID()
 	bytes, err := proto.Marshal(response)
