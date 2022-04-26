4712bb1c339cf151fc55d71657e471a7d0749bb2
NGUYEN Hong San <hongsan@gmail.com>
2022-04-26 17:18:42 +0700

    minor update

--- scyna/event.go
+++ scyna/event.go
@@ -29,12 +29,6 @@ func RegisterEvent[R proto.Message](channel string, consumer string, handler Eve
 	}
 }
 
-func PostEmptyEvent(channel string) {
-	if _, err := JetStream.Publish(channel, nil); err != nil {
-		log.Print(err.Error())
-	}
-}
-
 func PostEvent(channel string, event proto.Message) {
 	data, err := proto.Marshal(event)
 	if err != nil {
