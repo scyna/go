e137e03828a7ebec397bd9c0da1235168f68620e
NGUYEN Hong San <hongsan@gmail.com>
2022-04-28 17:37:40 +0700

    update querypool

--- example/scylla/user/scylla.go
+++ example/scylla/user/scylla.go
@@ -2,18 +2,23 @@ package user
 
 import (
 	"log"
+	"sync"
 
+	"github.com/scylladb/gocqlx/v2"
 	"github.com/scylladb/gocqlx/v2/qb"
 	"github.com/scyna/go/scyna"
 )
 
 type ScyllaRepository struct {
+	GetQueries *scyna.QueryPool
 }
 
 var Repository IRepository
 
 func InitScyllaRepository() {
-	Repository = &ScyllaRepository{}
+	Repository = &ScyllaRepository{
+		GetQueries: &scyna.QueryPool{sync.Pool{New: func() interface{} { return newGetQuery() }}},
+	}
 }
 
 func (r *ScyllaRepository) Create(LOG scyna.Logger, user *User) *scyna.Error {
@@ -43,15 +48,20 @@ func (r *ScyllaRepository) Exist(LOG scyna.Logger, email string) *scyna.Error {
 	return USER_NOT_EXISTED
 }
 
-func (r *ScyllaRepository) GetByEmail(LOG scyna.Logger, email string) (*scyna.Error, *User) {
-	var user User
-	if err := qb.Select("ex.user").
+func newGetQuery() *gocqlx.Queryx {
+	return qb.Select("ex.user").
 		Columns("id", "name", "email", "password").
 		Where(qb.Eq("email")).
 		Limit(1).
-		Query(scyna.DB).
-		Bind(email).
-		GetRelease(&user); err == nil {
+		Query(scyna.DB)
+}
+
+func (r *ScyllaRepository) GetByEmail(LOG scyna.Logger, email string) (*scyna.Error, *User) {
+	var query = r.GetQueries.GetQuery()
+	defer r.GetQueries.PutQuery(query)
+
+	var user User
+	if err := query.Bind(email).GetRelease(&user); err == nil {
 		return nil, &user
 	} else {
 		log.Print(err)
