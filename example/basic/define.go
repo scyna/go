8dcb0f856cf624f1f085414b42fea71c15e8eb68
NGUYEN Hong San <hongsan@gmail.com>
2022-04-28 15:10:39 +0700

    add urls

---
+++ example/basic/define.go
@@ -0,0 +1,7 @@
+package basic
+
+const (
+	HELLO_URL = "/example/basic/hello"
+	ECHO_URL  = "/example/basic/echo"
+	ADD_URL   = "/example/basic/add"
+)
--- example/basic_test/service_test.go
+++ example/basic_test/service_test.go
@@ -10,22 +10,22 @@ import (
 
 func TestHello(t *testing.T) {
 	response := basic.HelloResponse{Text: "Hello World"}
-	scyna_test.TestService(t, "/example/basic/hello", nil, &response, 200)
+	scyna_test.TestService(t, basic.HELLO_URL, nil, &response, 200)
 }
 
 func TestEcho(t *testing.T) {
 	request := basic.EchoRequest{Text: "echo"}
 	response := basic.EchoResponse{Text: "echo"}
-	scyna_test.TestService(t, "/example/basic/echo", &request, &response, 200)
+	scyna_test.TestService(t, basic.ECHO_URL, &request, &response, 200)
 }
 
 func TestAdd(t *testing.T) {
 	request := basic.AddRequest{A: 5, B: 73}
 	response := basic.AddResponse{Sum: 78}
-	scyna_test.TestService(t, "/example/basic/add", &request, &response, 200)
+	scyna_test.TestService(t, basic.ADD_URL, &request, &response, 200)
 }
 
 func TestAddTooBig(t *testing.T) {
 	request := basic.AddRequest{A: 50, B: 73}
-	scyna_test.TestService(t, "/example/basic/add", &request, scyna.REQUEST_INVALID, 400)
+	scyna_test.TestService(t, basic.ADD_URL, &request, scyna.REQUEST_INVALID, 400)
 }
