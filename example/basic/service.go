a663a304ddb6abb11aa07604e99242a7af006541
NGUYEN Hong San <hongsan@gmail.com>
2022-04-28 21:34:29 +0700

    refactoring protobuf

--- example/basic/service.go
+++ example/basic/service.go
@@ -1,23 +1,26 @@
 package basic
 
-import "github.com/scyna/go/scyna"
+import (
+	"github.com/scyna/go/example/basic/proto"
+	"github.com/scyna/go/scyna"
+)
 
 func Hello(ctx *scyna.Context) {
 	ctx.LOG.Info("Receive HelloRequest")
-	ctx.Done(&HelloResponse{Text: "Hello World"})
+	ctx.Done(&proto.HelloResponse{Text: "Hello World"})
 }
 
-func Echo(ctx *scyna.Context, request *EchoRequest) {
+func Echo(ctx *scyna.Context, request *proto.EchoRequest) {
 	ctx.LOG.Info("Receive EchoRequest")
-	ctx.Done(&EchoResponse{Text: request.Text})
+	ctx.Done(&proto.EchoResponse{Text: request.Text})
 }
 
-func Add(ctx *scyna.Context, request *AddRequest) {
+func Add(ctx *scyna.Context, request *proto.AddRequest) {
 	ctx.LOG.Info("Receive AddRequest")
 	sum := request.A + request.B
 	if sum > 100 {
 		ctx.Error(scyna.REQUEST_INVALID)
 	} else {
-		ctx.Done(&AddResponse{Sum: sum})
+		ctx.Done(&proto.AddResponse{Sum: sum})
 	}
 }
