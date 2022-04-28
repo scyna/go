6318f53924a02473de9ef61dd7c496da4518c9ea
NGUYEN Hong San <hongsan@gmail.com>
2022-04-28 10:53:27 +0700

    add validation

--- example/scylla/user/create.go
+++ example/scylla/user/create.go
@@ -1,13 +1,19 @@
 package user
 
 import (
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
 	"github.com/scyna/go/example/scylla/proto"
 	"github.com/scyna/go/scyna"
 )
 
 func Create(ctx *scyna.Context, request *proto.CreateUserRequest) {
 	ctx.LOG.Info("Receive CreateRequest")
-	/*TODO validate*/
+	if err := validateCreateRequest(request.User); err != nil {
+		ctx.Error(scyna.REQUEST_INVALID)
+		return
+	}
+
 	if err := Repository.Exist(ctx.LOG, request.User.Email); err == nil {
 		ctx.Error(USER_EXISTED)
 		return
@@ -20,4 +26,11 @@ func Create(ctx *scyna.Context, request *proto.CreateUserRequest) {
 		return
 	}
 	ctx.Done(&proto.CreateUserResponse{Id: user.ID})
+}
+
+func validateCreateRequest(user *proto.User) error {
+	return validation.ValidateStruct(user,
+		validation.Field(&user.Email, validation.Required, is.Email),
+		validation.Field(&user.Password, validation.Required, validation.Length(5, 10)),
+		validation.Field(&user.Name, validation.Required, validation.Length(1, 100)))
 }
