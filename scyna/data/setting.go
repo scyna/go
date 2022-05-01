3c518bad325e58d821a545e012b1ce1d792a1002
NGUYEN Hong San <hongsan@gmail.com>
2022-05-01 21:34:00 +0700

    remove command

--- scyna/data/setting.go
+++ scyna/data/setting.go
@@ -20,7 +20,7 @@ func WriteSetting(module string, key string, value string) {
 		}
 	}
 
-	scyna.SendCommand(scyna.SETTING_UPDATE_CHANNEL+module, &scyna.SettingUpdatedSignal{
+	scyna.EmitSignal(scyna.SETTING_UPDATE_CHANNEL+module, &scyna.SettingUpdatedSignal{
 		Key:   key,
 		Value: value,
 	})
