4df3974f47312a05f7a3c458eda11216cbb3210a
NGUYEN Hong San <hongsan@gmail.com>
2022-05-10 21:42:47 +0700

    ServiceDone

--- engine/manager/trace/service_done.go
+++ engine/manager/trace/service_done.go
@@ -1,14 +1,20 @@
 package trace
 
 import (
+	"fmt"
 	"log"
 
+	"github.com/scylladb/gocqlx/v2/qb"
 	"github.com/scyna/go/scyna"
 )
 
 func ServiceDone(signal *scyna.ServiceDoneSignal) {
 	log.Print("Service Done")
-	log.Print(signal.Request)
-	log.Print(signal.Response)
-	/* TODO */
+	data := fmt.Sprintf("{Status:%d}\n%s\n%s", signal.Status, signal.Request, signal.Response)
+	log.Print(data)
+	qb.Insert("scyna.tag").
+		Columns("trace_id", "key", "value").
+		Query(scyna.DB).
+		Bind(signal.TraceID, "data", data).
+		ExecRelease()
 }
