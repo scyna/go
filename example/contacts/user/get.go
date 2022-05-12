d5e1814e963fbfa6e2c99417fe14b1ba3c9e3f8a
NGUYEN Hong San <hongsan@gmail.com>
2022-05-12 17:11:30 +0700

    minor update

--- example/contacts/user/get.go
+++ example/contacts/user/get.go
@@ -5,12 +5,12 @@ import (
 	"github.com/scyna/go/scyna"
 )
 
-func Get(ctx *scyna.Service, request *proto.GetUserRequest) {
-	ctx.Logger.Info("Receive GetUserRequest")
-	if err, user := Repository.GetByEmail(ctx.Logger, request.Email); err != nil {
-		ctx.Error(err)
+func Get(s *scyna.Service, request *proto.GetUserRequest) {
+	s.Logger.Info("Receive GetUserRequest")
+	if err, user := Repository.GetByEmail(s.Logger, request.Email); err != nil {
+		s.Error(err)
 		return
 	} else {
-		ctx.Done(&proto.GetUserResponse{User: user.ToDTO()})
+		s.Done(&proto.GetUserResponse{User: user.ToDTO()})
 	}
 }
