8205c222d29a1aac6ea8a3d2119366010088344c
Dang Viet Hung <hungdv48@vingroup.net>
2022-06-14 13:48:46 +0700

    update task

--- engine/manager/task/add_recurring.go
+++ engine/manager/task/add_recurring.go
@@ -5,4 +5,5 @@ import "github.com/scyna/go/scyna"
 func AddRecurringTask(s *scyna.Service, request *scyna.AddRecurringTaskRequest) {
 	// TODO: Check validate task
 	// TODO: Insert task to scyna.recurring_task table
+	// TODO: Insert task to scyna.task table
 }
