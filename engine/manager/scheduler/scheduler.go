583a1d3518472dec13d882c83dc9c1e26858c509
NGUYEN Hong San <hongsan@gmail.com>
2022-06-17 09:19:33 +0700

    scheduler cont

--- engine/manager/scheduler/scheduler.go
+++ engine/manager/scheduler/scheduler.go
@@ -1,6 +1,12 @@
 package scheduler
 
-import "time"
+import (
+	"log"
+	"time"
+
+	"github.com/scylladb/gocqlx/v2/qb"
+	"github.com/scyna/go/scyna"
+)
 
 const INTERVAL = 30 * time.Second
 const CLEANUP_FACTOR = 10
@@ -8,6 +14,17 @@ const CLEANUP_FACTOR = 10
 var ticker *time.Ticker
 var done = make(chan bool)
 var cleanupCount = 0
+
+type task struct {
+	ID        uint64
+	Topic     string
+	Data      []byte
+	Running   bool
+	LoopCount uint64
+	LoopMax   uint64
+	Next      time.Time
+	Interval  uint64
+}
 
 func Start() {
 	ticker = time.NewTicker(INTERVAL)
@@ -21,7 +38,6 @@ func Start() {
 			}
 		}
 	}()
-
 }
 
 func Stop() {
@@ -30,10 +46,10 @@ func Stop() {
 }
 
 func loop() {
-	var bucket uint64
+	bucket := bucket(time.Now())
 	for {
 		tasks := todos(bucket)
-		if len(tasks) == 0 {
+		if tasks == nil {
 			break
 		}
 		for _, t := range tasks {
@@ -49,14 +65,64 @@ func loop() {
 }
 
 func todos(bucket uint64) []uint64 {
-	/*TODO*/
-	return nil
+	var ret []uint64
+	if err := qb.Select("scyna.todo").
+		Columns("id").
+		Where(qb.Eq("bucket")).
+		Limit(20).
+		Query(scyna.DB).
+		Bind(bucket).
+		SelectRelease(ret); err != nil {
+		return nil
+	}
+
+	if len(ret) == 0 {
+		return nil
+	}
+
+	return ret
 }
 
-func process(task uint64) {
-	/*TODO*/
+func process(id uint64) {
+	var t task
+	if err := qb.Select("scyna.task").
+		Columns("id", "topic", "data", "next", "interval", "loop_count", "loop_max", "running").
+		Where(qb.Eq("id")).
+		Limit(1).
+		Query(scyna.DB).
+		Bind(id).
+		GetRelease(t); err != nil {
+		log.Print("Can not load task")
+		return
+	}
+	if !t.Running {
+		/*TODO: do nothing, remove task from todo list only*/
+		return
+	}
+
+	scyna.JetStream.Publish(t.Topic, t.Data)
+
+	t.LoopCount++
+	if t.LoopCount < t.LoopMax {
+		/*TODO: calculate next*/
+		/*Batch:
+		1. remove old task on todolist
+		2. add new task to todo list
+		3. update task
+		*/
+	} else {
+		t.Running = false
+		/*Batch:
+		1. remove old task on todolist
+		3. update task
+		*/
+	}
 }
 
 func cleanup() {
 	/*TODO*/
+}
+
+func bucket(time time.Time) uint64 {
+	return uint64(time.Unix() / 60)
 }
