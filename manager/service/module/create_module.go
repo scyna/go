d338e26458ca702598b189c09e95f2253c62c1ff
NGUYEN Hong San <hongsan@gmail.com>
2022-07-08 09:49:43 +0700

    minor update

--- manager/service/module/create_module.go
+++ manager/service/module/create_module.go
@@ -8,7 +8,10 @@ import (
 func CreateModule(s *scyna.Service, request *proto.Module) {
 	s.Logger.Info("Receive CreateModuleRequest")
 
-	/*TODO*/
+	/*TODO: check input*/
+	/*TODO: save module to database */
+	/*TODO: create stream on NATS for sync */
+	/*TODO: create stream on NATS for event */
 
 	s.Done(scyna.OK)
 }
