1cbb2f1ab29a7f8f79a7be2bfaddd1e35856983a
NGUYEN Hong San <hongsan@gmail.com>
2022-07-09 10:50:33 +0700

    minor update

--- manager/.proto/generated/module.pb.go
+++ manager/.proto/generated/module.pb.go
@@ -91,53 +91,6 @@ func (x *Module) GetSecret() string {
 	return ""
 }
 
-type SetupModuleDatabaseRequest struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	Module string `protobuf:"bytes,1,opt,name=module,proto3" json:"module,omitempty"`
-}
-
-func (x *SetupModuleDatabaseRequest) Reset() {
-	*x = SetupModuleDatabaseRequest{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_module_proto_msgTypes[1]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *SetupModuleDatabaseRequest) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*SetupModuleDatabaseRequest) ProtoMessage() {}
-
-func (x *SetupModuleDatabaseRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_module_proto_msgTypes[1]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use SetupModuleDatabaseRequest.ProtoReflect.Descriptor instead.
-func (*SetupModuleDatabaseRequest) Descriptor() ([]byte, []int) {
-	return file_module_proto_rawDescGZIP(), []int{1}
-}
-
-func (x *SetupModuleDatabaseRequest) GetModule() string {
-	if x != nil {
-		return x.Module
-	}
-	return ""
-}
-
 var File_module_proto protoreflect.FileDescriptor
 
 var file_module_proto_rawDesc = []byte{
@@ -150,11 +103,8 @@ var file_module_proto_rawDesc = []byte{
 	0x69, 0x70, 0x74, 0x69, 0x6f, 0x6e, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0b, 0x64, 0x65,
 	0x73, 0x63, 0x72, 0x69, 0x70, 0x74, 0x69, 0x6f, 0x6e, 0x12, 0x16, 0x0a, 0x06, 0x73, 0x65, 0x63,
 	0x72, 0x65, 0x74, 0x18, 0x04, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x73, 0x65, 0x63, 0x72, 0x65,
-	0x74, 0x22, 0x34, 0x0a, 0x1a, 0x53, 0x65, 0x74, 0x75, 0x70, 0x4d, 0x6f, 0x64, 0x75, 0x6c, 0x65,
-	0x44, 0x61, 0x74, 0x61, 0x62, 0x61, 0x73, 0x65, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12,
-	0x16, 0x0a, 0x06, 0x6d, 0x6f, 0x64, 0x75, 0x6c, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52,
-	0x06, 0x6d, 0x6f, 0x64, 0x75, 0x6c, 0x65, 0x42, 0x0c, 0x48, 0x02, 0x5a, 0x08, 0x2e, 0x2f, 0x3b,
-	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x74, 0x42, 0x0c, 0x48, 0x02, 0x5a, 0x08, 0x2e, 0x2f, 0x3b, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62,
+	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -169,10 +119,9 @@ func file_module_proto_rawDescGZIP() []byte {
 	return file_module_proto_rawDescData
 }
 
-var file_module_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_module_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
 var file_module_proto_goTypes = []interface{}{
-	(*Module)(nil),                     // 0: proto.Module
-	(*SetupModuleDatabaseRequest)(nil), // 1: proto.SetupModuleDatabaseRequest
+	(*Module)(nil), // 0: proto.Module
 }
 var file_module_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -200,18 +149,6 @@ func file_module_proto_init() {
 				return nil
 			}
 		}
-		file_module_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*SetupModuleDatabaseRequest); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -219,7 +156,7 @@ func file_module_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_module_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
