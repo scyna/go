7673c2a5e77be2f0a6143dc86e1e346bef93fe75
KhanhND <khanhnd162@gmail.com>
2022-07-11 10:42:32 +0700

    nats tool

--- manager/utils/nats.go
+++ manager/utils/nats.go
@@ -1,11 +1,41 @@
 package utils
 
+import (
+	"errors"
+	"github.com/nats-io/nats.go"
+	"github.com/scyna/go/scyna"
+	"log"
+	"time"
+)
+
+func DeleteStream(name string) error {
+	err := scyna.JetStream.DeleteStream(name)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func CreateStream(name string) error {
-	/*TODO*/
+	if _, err := scyna.JetStream.AddStream(&nats.StreamConfig{
+		Name:     name,
+		Subjects: []string{name + ".*"},
+		Storage:  nats.FileStorage,
+		MaxAge:   time.Hour * 24 * 7, //keep for a week
+	}); err != nil {
+		return errors.New("CreateStream " + name + ": Error: " + err.Error())
+	}
 	return nil
 }
 
 func CreateConsumer(stream string, name string, channel string) error {
-	/*TODO*/
+	if _, err := scyna.JetStream.AddConsumer(stream, &nats.ConsumerConfig{
+		Durable:      channel,
+		DeliverGroup: name,
+		//DeliverSubject: deliverSubject,
+		// FilterSubject:  filterSubject,
+	}); err != nil {
+		log.Print("Add Consumer "+channel+": Error: ", err)
+	}
 	return nil
 }
