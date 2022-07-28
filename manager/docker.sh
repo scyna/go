67927daebefd17e15900f9acc2bf040d512998a9
Dang Viet Hung <hungdv48@vingroup.net>
2022-07-28 08:43:43 +0700

    update docker

--- manager/docker.sh
+++ manager/docker.sh
@@ -1,4 +1,9 @@
-image=scyna/manager:$1
-
-docker build -t ${image} -f Dockerfile .
-docker push ${image}
+#!/bin/bash
+if [ "$1" != '' ];
+then
+    image=registry.vin3s.vn:5000/scyna/manager:$1
+    docker build -t ${image} -f Dockerfile .
+    docker push ${image}
+else
+    echo 'Please pass version of image!'
+fi
