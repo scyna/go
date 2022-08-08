4370c5ff4316f3daeb88374df886ea8d62d49f67
Dang Viet Hung <hungdv48@vingroup.net>
2022-08-08 08:46:16 +0700

    add activity

--- scyna/activity.go
+++ scyna/activity.go
@@ -1,53 +1,29 @@
 package scyna
 
 import (
-	"time"
-
 	"github.com/scylladb/gocqlx/v2"
 	"github.com/scylladb/gocqlx/v2/qb"
-	"google.golang.org/protobuf/proto"
-	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
+	"github.com/scyna/go/scyna"
 )
-
-const tryCount = 10
 
 var activityPool *QueryPool
 
 func SetupActivity(table string) {
 	activityPool = NewQueryPool(func() *gocqlx.Queryx {
-		return qb.Insert(table).
-			Columns("entity_id", "type", "time", "data").
+		return qb.Insert(module+".activity").
+			Columns("entity_id", "event_id").
 			Unique().
-			Query(DB)
+			Query(scyna.DB)
 	})
 }
 
-func AddActivity(entity uint64, Type int32, activity protoreflect.ProtoMessage) {
-	if activityPool == nil {
-		LOG.Error("Setup activity table first")
-		return
-	}
-
-	t := time.Now()
-
-	var data []byte
-	if activity != nil {
-		data, _ = proto.Marshal(activity)
-	}
-
+func addActivity(entityID uint64, eventID int64) {
 	qInsert := activityPool.GetQuery()
-	defer activityPool.Put(qInsert)
 
-	for i := 0; i < tryCount; i++ {
-		qInsert.Bind(entity, Type, t, data)
-		if applied, err := qInsert.ExecCAS(); applied {
-			return
-		} else {
-			if err != nil {
-				LOG.Error("ActivityStream.Add :" + err.Error())
-				return
-			}
-		}
-		t.Add(time.Millisecond + 1)
+	if applied, err := qInsert.Bind(entityID, eventID).ExecCAS(); applied {
+		return
+	} else if err != nil {
+		LOG.Error("addActivity" + err.Error())
+		return
 	}
 }
