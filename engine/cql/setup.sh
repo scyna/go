3c9a2253cbeed4a53830372046f31607481de5ba
Dang Viet Hung <hungdv48@vingroup.net>
2022-08-11 14:59:59 +0700

    minor update

--- engine/cql/setup.sh
+++ engine/cql/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-host=db-oneprofile.vin3s.vn
-username=cassandra
-password=cassandra
+host=dbstg-op.vin3s.vn
+username=scylla
+password=scylla
 
 files='_cleanup.cql generator.cql module.cql session.cql trace.cql client.cql organization.cql application.cql task.cql data.cql'
 
