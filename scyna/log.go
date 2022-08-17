24523a466dc9fe5bb3b5db2a0b5ba465ecde7b61
Dang Viet Hung <hungdv48@vingroup.net>
2022-08-17 10:04:32 +0700

    update log

--- scyna/log.go
+++ scyna/log.go
@@ -39,28 +39,26 @@ func UseDirectLog(count int) {
 	logQueue = make(chan LogData)
 
 	for i := 0; i < count; i++ {
-		qSession := qb.Insert("scyna.session_log").Columns("session_id", "day", "time", "seq", "level", "message").Unique().Query(DB)
-		qService := qb.Insert("scyna.log").Columns("trace_id", "time", "seq", "level", "message").Unique().Query(DB)
+		qSession := qb.Insert("scyna.session_log").Columns("session_id", "day", "time", "seq", "level", "message").Unique()
+		qService := qb.Insert("scyna.log").Columns("trace_id", "time", "seq", "level", "message").Unique()
 
 		go func() {
 			for l := range logQueue {
 				time_ := time.Now()
 				if l.Session {
-					if _, err := qSession.
+					if _, err := qSession.Query(DB).
 						Bind(l.ID, GetDayByTime(time_), time_, l.Sequence, l.Level, l.Message).
 						ExecCAS(); err != nil {
 						log.Println("saveSessionLog: " + err.Error())
 					}
 				} else {
-					if _, err := qService.
+					if _, err := qService.Query(DB).
 						Bind(l.ID, time_, l.Sequence, l.Level, l.Message).
 						ExecCAS(); err != nil {
 						log.Println("saveServiceLog: " + err.Error())
 					}
 				}
 			}
-			qService.Release()
-			qSession.Release()
 		}()
 	}
 }
