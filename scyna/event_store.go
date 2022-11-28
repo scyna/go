0bb2de97016ab52f8b6ba51db0491cb22af7f599
NGUYEN Hong San <hongsan@gmail.com>
2022-11-28 12:15:28 +0700

    event store

--- scyna/event_store.go
+++ scyna/event_store.go
@@ -1,26 +1,53 @@
 package scyna
 
-import "google.golang.org/protobuf/proto"
+import (
+	"fmt"
+	"log"
+
+	"github.com/scylladb/gocqlx/v2/qb"
+	"google.golang.org/protobuf/proto"
+)
 
 type eventStore struct {
-	Version uint64
+	version       uint64
+	esQuery       string
+	activityQuery string
 }
 
-func NewEventStore() *eventStore {
-	/*TODO: load lastest version from database*/
+func InitEventStore(name string) *eventStore {
+	var version uint64 = 0
+
+	esTable := fmt.Sprintf("%s.%s_event_store", context, name)
+
+	if err := qb.Select(esTable).
+		Columns("max(event_id)"). //FIXME
+		Limit(1).
+		Query(DB).
+		GetRelease(&version); err != nil {
+		log.Fatal("Can not init EventStore")
+	}
+
 	return &eventStore{
-		Version: 0, /*FIXME*/
+		version:       version,
+		esQuery:       fmt.Sprintf("INSERT INTO %s.%s_event_store(event_id, aggregate_id, channel, data) VALUES(?,?,?,?)", context, name),
+		activityQuery: fmt.Sprintf("INSERT INTO %s.%s_activity(aggregate_id, event_id) VALUES(?,?)", context, name),
 	}
 }
 
 func (es *eventStore) Add(ctx *Command, aggregate uint64, channel string, event proto.Message) bool {
-	/*TODO: add event to EventStore (batch)*/
-	var v = es.Version + 1
+	var id = es.version + 1
 
-	ctx.batch.Query("TODO")
+	bytes, err := proto.Marshal(event)
+	if err != nil {
+		ctx.Logger.Error("Can not marshal event data")
+		return false
+	}
+
+	ctx.batch.Query(es.esQuery, id, aggregate, channel, bytes)
+	ctx.batch.Query(es.activityQuery, aggregate, id)
 
 	if err := DB.ExecuteBatch(ctx.batch); err == nil {
-		es.Version = v
+		es.version = id
 		return true
 	}
 
