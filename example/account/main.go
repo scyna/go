7de77ed6eb283c698cce41cb789f0c3ea07f275e
Nguyễn Thế Công <v.congnt33@vin3s.vn>
2023-02-02 14:28:34 +0700

    init account

---
+++ example/account/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/scyna/go/scyna"
+)
+
+const MODULE_CODE = "account"
+
+func main() {
+	secret_ := flag.String("password", "123456789aA@#", "Authenticate By Token")
+	managerUrl := flag.String("managerUrl", "http://127.0.0.1:8081", "Manager Url")
+	flag.Parse()
+
+	scyna.RemoteInit(scyna.RemoteConfig{
+		ManagerUrl: *managerUrl,
+		Name:       MODULE_CODE,
+		Secret:     *secret_,
+	})
+	scyna.UseRemoteLog(3)
+	defer scyna.Release()
+
+	scyna.Start()
+}
